8da475dcf8b8f30bbc04912b3c2e7fa82acd96dc
Tasnuva Haque <A01174842@ad.bcit.ca>
2019-11-01 12:56:43 -0700

    responsive nav added

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,47 @@
-// Write your JavaScript here...
+// JavaScript Document
+
+// Responsive Menu - Dropdown
+const body = document.body;
+const btnMenu = document.getElementById('hamburger');
+const nav = document.getElementById('main-nav');
+
+btnMenu.addEventListener('click', openMenu);
+// Prevents the focus state from activating
+btnMenu.addEventListener('mousedown', function(e){
+    e.preventDefault();
+});
+
+function openMenu(){
+    document.body.classList.toggle('show');
+	nav.classList.add('activated');
+}
+
+// Create a media query list using
+// matchMedia
+const mql = window.matchMedia('(min-width: 560px)');
+
+// Add a Media Query Listener to the 
+// above media query list
+mql.addListener(removeTransition);
+
+// Function to remove the transition
+// from the navigation when the user
+// resizes the browser to desktop
+// sizes. In this case when the
+// screen width becomes wider then
+// 560px
+function removeTransition(e){
+	// e -> is the event object
+	// e.matches -> stores a true false
+	// value depending if the media query
+	// list set above matches or not
+	if(e.matches){
+		body.classList.remove('show');
+		//nav.classList.remove('activated');
+	}
+}
+
+
+
+
+
